262e79a886a8b662b3aeef410fac9ebedc9e703b
BigMickey <williamclivestevens1205@gmail.com>
2025-02-28 16:57:01 +0800

    Implement q_size functionality
    - Used list_for_each macro to traverse the queue
    
    Change-Id: Ic40b786a44909674212f37703d59a9cbe95eeef3

--- queue.c
+++ queue.c
@@ -46,7 +46,13 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    struct list_head *pos;
+    short int cnt = 0;
+
+    list_for_each (pos, head) {
+        cnt++;
+    }
+    return cnt;
 }
 
 /* Delete the middle node in queue */
