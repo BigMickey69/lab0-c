8a76a583b68ea552ea0ae08f0a613e131d1867f2
BigMickey69 <williamclivestevens1205@gmail.com>
2025-03-02 10:37:00 +0800

    Implement q_reverse
    - goes through the list swapping *next and *prev pointers
    
    Change-Id: Id89cea02412c64ca708368888faa6a0179f5a254

--- queue.c
+++ queue.c
@@ -142,7 +142,19 @@ void q_swap(struct list_head *head)
 }
 
 /* Reverse elements in queue */
-void q_reverse(struct list_head *head) {}
+void q_reverse(struct list_head *head)
+{
+    if (!head || list_empty(head))
+        return;
+
+    struct list_head *p = head;
+    do {
+        struct list_head *temp = p->next;
+        p->next = p->prev;
+        p->prev = temp;
+        p = temp;
+    } while (p != head);
+}
 
 /* Reverse the nodes of the list k at a time */
 void q_reverseK(struct list_head *head, int k)
